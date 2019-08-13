2e9899d410c1409653ceb0733c3af4c5ea1576e0
wangzai9999 <15639768781@163.com>
2019-08-13 13:59:08 +0800

    s

---
+++ src/main/java/com/dao/PositionDao.java
@@ -0,0 +1,18 @@
+package com.dao;
+
+import com.entity.Position;
+
+import java.util.List;
+
+public interface PositionDao {
+    public List<Position> list();
+
+    public void add(Position p);
+
+    public void del(Long id);
+
+    public  void mod(Position p);
+
+    public Position getOne(Long id);
+
+}
