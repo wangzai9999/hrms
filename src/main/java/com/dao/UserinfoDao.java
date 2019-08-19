be28856804fc8a987d1d8aafac753bce01fa95e9
wangzai9999 <15639768781@163.com>
2019-08-19 17:37:12 +0800

    s

--- src/main/java/com/dao/UserinfoDao.java
+++ src/main/java/com/dao/UserinfoDao.java
@@ -51,7 +51,4 @@ public interface UserinfoDao {
 
     @Select("select us_seq.currval from dual")
     public Long getNowID();
-
-    @Select("select * from userinfo where us_dep=#{id}")
-    public List<Userinfo> getAllByDep(Long id);
 }
